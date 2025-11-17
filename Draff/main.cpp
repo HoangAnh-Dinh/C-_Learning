e2da0107022b0786bb0640b8b6b91de51f52dde3
AnhDXH <dinhhoanganh714@gmail.com>
2025-11-17 07:35:20 +0700

    AnhDXH day 1117

--- Draff/main.cpp
+++ Draff/main.cpp
@@ -5,13 +5,32 @@
 
 using namespace std;
 
+void swap(int& a, int& b)
+{
+a = a^b;
+b = a^b;
+a = a^b;
+}
 int main()
 {
-int a[10] = {1,2,3,4,5,6,7,8,9,10};
-remove(a,a+10,7);
-for(auto e:a)
+int a[10] = {7,6,4,5,9,3,1,0,2,10};
+int n = sizeof(a)/sizeof(a[0]);
+int minIdx = 0;
+for(int i = 0; i < n-1; i++)
 {
-    cout<<e<<endl;
+minIdx = i;
+for(int j = i+1; j < n-1; j++)
+{
+ if(a[j]<a[minIdx])
+ {
+    swap(a[j],a[minIdx]);
+ }
+}
+}
+
+for(auto x:a)
+{
+ cout<<x<<endl;
 }
 return 0;
 }
