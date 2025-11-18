c4f0e66a4254c122166d8ce3755456f2907ae225
AnhDXH <dinhhoanganh714@gmail.com>
2025-11-18 21:15:33 +0700

    AnhDXH day CL3

--- Draff/main.cpp
+++ Draff/main.cpp
@@ -1,36 +1,18 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
 
-using namespace std;
+//Con tro hang va hang con tro
 
-void swap(int& a, int& b)
-{
-a = a^b;
-b = a^b;
-a = a^b;
-}
+
 int main()
 {
-int a[10] = {7,6,4,5,9,3,1,0,2,10};
-int n = sizeof(a)/sizeof(a[0]);
-int minIdx = 0;
-for(int i = 0; i < n-1; i++)
-{
-minIdx = i;
-for(int j = i+1; j < n-1; j++)
-{
- if(a[j]<a[minIdx])
- {
-    swap(a[j],a[minIdx]);
- }
-}
-}
+int a = 8;
+int c = 10;
+int* const b = &a;
 
-for(auto x:a)
-{
- cout<<x<<endl;
-}
+a = 15;
+
+std::cout<<" *b = "<<*b;
+
+
 return 0;
 }
